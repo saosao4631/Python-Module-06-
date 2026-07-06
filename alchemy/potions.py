ec8f3a31739ea8c40a901417773e1ab810a50389
ksaotome <ksaotome@student.42tokyo.jp>
2026-07-06 20:35:08 +0900

    aaa

---
+++ alchemy/potions.py
@@ -0,0 +1,15 @@
+import elements
+from .elements import create_air, create_earth
+
+
+def healing_potion() -> str:
+    return (
+        f"Healing potion brewed with '{create_earth()}' "
+        f"and '{create_air()}'"
+    )
+
+def strength_potion() -> str:
+    return (
+        f"Strength potion brewed with '{elements.reate_fire()}' "
+        f"and '{elements.create_water()}'"
+    )
